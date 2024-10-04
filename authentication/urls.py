6370919fc714f2bd41c8046cc755a1484d0905ed
ANONYMOUS <ANONYMOUS>
2024-10-04 12:48:11 +0200

    successfullly added some icons

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import signup, signin, dashboard, activate_account, home  # Import the home view
+from django.contrib.auth.views import LogoutView  # Import LogoutView
 
 urlpatterns = [
     path('', home, name='home'),  # Set the home view as the root URL
@@ -7,4 +8,7 @@ urlpatterns = [
     path('signin/', signin, name='signin'),
     path('dashboard/', dashboard, name='dashboard'),
     path('activate/<int:pk>/', activate_account, name='activate_account'),
+    
+    # Add the logout URL
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
 ]
