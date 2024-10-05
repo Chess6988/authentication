fe27f500323c466ec94e90c15d5a606d8ae06ea2
ANONYMOUS <ANONYMOUS>
2024-10-05 18:38:28 +0200

    successfulll  added motioon image and added dashboard as root url

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import signup, signin, dashboard, activate_account, home  # Import the home view
-from django.contrib.auth.views import LogoutView  # Import LogoutView
+from .views import signup, signin, dashboard, activate_account, home
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', home, name='home'),  # Set the home view as the root URL
+    path('', dashboard, name='dashboard'),  # Set the dashboard view as the root URL
     path('signup/', signup, name='signup'),
     path('signin/', signin, name='signin'),
-    path('dashboard/', dashboard, name='dashboard'),
+    path('home/', home, name='home'),  # You can still keep the home view accessible via '/home/'
     path('activate/<int:pk>/', activate_account, name='activate_account'),
     
     # Add the logout URL
