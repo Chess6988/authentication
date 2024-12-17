56a153caa1273c0d98137eee40cfa0af87ae658a
ANONYMOUS <ANONYMOUS>
2024-12-17 10:49:32 +0100

    Fixed the issue of truck details not showing in the modal

--- camions/admin.py
+++ camions/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Truck
+from .models import Truck, RubberTransport  # Import the new model
 
 @admin.register(Truck)
 class TruckAdmin(admin.ModelAdmin):
@@ -10,5 +10,12 @@ class TruckAdmin(admin.ModelAdmin):
 
 
 
+@admin.register(RubberTransport)
 class RubberTransportAdmin(admin.ModelAdmin):
-    list_display = ['truck', 'tons_of_rubber', 'price_per_ton']  # Ensure valid fields only
+    list_display = ('truck', 'number_of_trucks', 'tons_of_rubber', 'price_per_ton', 'date', 'total_price')  # Columns to display
+    search_fields = ('truck__matriculation_number',)  # Search by truck matriculation number
+    list_filter = ('date', 'truck')  # Filters to apply on the right side
+
+    def total_price(self, obj):
+        return obj.total_price()  # Call the method to display total price
+    total_price.short_description = 'Total Price'  # Set a short description for the column
